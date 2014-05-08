37c117a3a9243ea72cbbdf9c3e7be7a8b7e2f4c8
Simon Sun <blackghost0118@gmail.com>
2014-05-08 14:28:38 +0800

    QQ可以改圖好棒棒

--- FuckSimon/GraphicsMagick/test.js
+++ FuckSimon/GraphicsMagick/test.js
@@ -4,8 +4,11 @@ gm('./GoodMan.jpg')
   .font('w9.ttf')
   .fontSize(36)
   .drawText(30, 35, '幹你娘還要下載字型檔')
-  .write('./G1.jpg', function(err){
-    if(err) console.dir(err);
-    console.dir('Success!!!');
+  .write('./output/output.jpg', function(err){
+    if(err){
+      console.dir(err);
+      process.exit();
+    }
+    console.dir('Success, image in ./output/output.jpg');
     process.exit();
   });

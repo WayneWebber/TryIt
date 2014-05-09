abdb8ddfee0f77265250cbcf6d2d6c45696c0714
johnwuossii <superboxya@gmail.com>
2014-05-09 14:45:41 +0800

    更新

--- mac/ServerGetUrl/mac.js
+++ mac/ServerGetUrl/mac.js
@@ -10,7 +10,7 @@ var macbook_pro13 = 'http://store.apple.com/tw/browse/home/specialdeals/mac/macb
 var macbook_pro15 = 'http://store.apple.com/tw/browse/home/specialdeals/mac/macbook_pro/15';
 var mac = 'http://store.apple.com/tw/browse/home/specialdeals/clearance/mac';
 
-var html,pro13 = {},pro15 = {},air11 = {},air13 = {};
+var pro13 = {},pro15 = {},air11 = {},air13 = {};
 var head = '<div class="header">'
       +'<a href="http://store.apple.com/tw/browse/home/specialdeals/mac">MAC官網</a>'
       +'<a href="http://store.apple.com/tw/browse/home/specialdeals/mac/macbook_air">macAir</a>'
@@ -28,41 +28,7 @@ var css = '<style>'
       +'.header a{font:bold 15px Arial, Helvetica, sans-serif;font-style:normal;color:#ffffff;background:#000000;border:0px solid #ffffff;text-shadow:0px 0px 0px #222222;box-shadow:0px 0px 0px #000000;-moz-box-shadow:0px 0px 0px #000000;-webkit-box-shadow:0px 0px 0px #000000;border-radius:10px 10px 10px 10px;-moz-border-radius:10px 10px 10px 10px;-webkit-border-radius:10px 10px 10px 10px;width:44px;padding:11px 35px;cursor:pointer;margin:0 5px;}'
       +'p.price{font-size:20px;background-color: #F9F2F4;color: #C7254E;padding:2px 4px;}'
       +'</style>'; 
-// request({ url: macbook_pro13}, function(err, resp, body) {
-//   if (!err && resp.statusCode == 200) {
-//     var $ = cheerio.load(body);
-//     pro13 = '<h2>Pro13</h2>' + $('.box-content').html() + '<div style="clear:left"></div>';
-//   }
-// });
-// request({ url: macbook_pro15}, function(err, resp, body) {
-//   if (!err && resp.statusCode == 200) {
-//     var $ = cheerio.load(body);
-//     pro15 = '<h2>Pro15</h2>' + $('.box-content').html() + '<div style="clear:left"></div>';
-//   }
-// });
-// request({ url: macbook_air11}, function(err, resp, body) {
-//   if (!err && resp.statusCode == 200) {
-//     var $ = cheerio.load(body);
-//     air11 = '<h2>Air11</h2>' + $('.box-content').html() + '<div style="clear:left"></div>';
-//   }
-// });
-// request({ url: macbook_air13}, function(err, resp, body) {
-//   if (!err && resp.statusCode == 200) {
-//     var $ = cheerio.load(body);
-//     air13 = '<h2>Air13</h2>' + $('.box-content').html() + '<div style="clear:left"></div>';
-//   }
-// });
-// app.get('/', function(req, res){
-//   res.send( css + head + pro13 + pro15 + air11 + air13  );
-// });
 
-
-
-var i = 0; 
-setInterval(function(){ 
-  console.log('count: %s', i);
-  i++;
-}, 1000);
 function macData(url, target, title){
   request({ url: url}, function(err, resp, body) {
     if (!err && resp.statusCode == 200) {
@@ -78,7 +44,7 @@ var intervalId = setInterval(function(){
   macData(macbook_pro13, pro13, 'Pro13');
   macData(macbook_pro15, pro15, 'Pro15');
   app.get('/', function(req, res){
-    res.send( css + head + air11.html + air13.html + pro13.html + pro15.html   );
+    res.send( css + head + air11.html + air13.html + pro13.html + pro15.html );
   });
 },360000);
 setTimeout(function(){ 
@@ -87,7 +53,7 @@ setTimeout(function(){
   macData(macbook_pro13, pro13, 'Pro13');
   macData(macbook_pro15, pro15, 'Pro15');
   app.get('/', function(req, res){
-    res.send( css + head + air11.html + air13.html + pro13.html + pro15.html   );
+    res.send( css + head + air11.html + air13.html + pro13.html + pro15.html );
   });
   app.listen(3000);
   console.log('is run 3000');
